c95b5fc39014aa49643c3febcbaa6714164af087
syun123456789 <kadosyun1997@gmail.com>
2024-07-28 00:46:44 +0900

    add register,trangit_select_user

--- app.py
+++ app.py
@@ -6,26 +6,41 @@ from flask import (Flask, redirect, render_template, request,
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
    print('Request for index page received')
    return render_template('index.html')
+   
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
+@app.route('/select_user', methods=['POST'])
+def transit_to_select_user_page():
+   name = request.form.get('task')
 
    if name:
        print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
+       return render_template('select_user.html', name = name)
    else:
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
+   
+@app.route('/registered', method=['post'])
+def registered():
+    worker = request.form.get('worker')
+    name = request.form.get('task')
+    
+    if worker:
+       print('Request for hello page received with name=%s' % worker)
+       return render_template('register.html', worker = worker, name = task)
+    else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
+
+
 
 
 if __name__ == '__main__':
